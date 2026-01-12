9c9c2aa964325888fd95e5a0ae530a2c6ebc9c9c
Colin Beeby <colin.beeby1@nhs.net>
2026-01-12 12:25:14 +0000

    updated version number

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025120170;     // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2026011200;     // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2025092600;    // Moodle 5.1.
 $plugin->component = 'format_tiles';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
